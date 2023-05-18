57e7f06583239667a39e8dddc450b0ca5b448f4d
TheFlash2k <alitaqi2000@gmail.com>
2023-05-19 03:15:56 +0500

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+	echo "[!] Please run the script as root!"
+	exit 1
+fi
+
+echo "[+] Raidware Setup script."
+echo "[+] Setting up mingw:"
+apt install mingw-w64-common mingw-w64-i686-dev mingw-w64-tools mingw-w64-x86-64-dev
+
+echo "[+] Setting up CSC for dotnet: "
+echo "[*] Downloading the microsoft prod package"
+(wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb) &>/dev/null
+sudo dpkg -i /tmp/packages-microsoft-prod.deb
+rm /tmp/packages-microsoft-prod.deb
+
+echo "[*] Updating the repositories"
+(apt update) &>/dev/null
+echo "[*] Install dotnet-sdk and dotnet-runtime"
+(apt install -y dotnet-sdk-7.0 dotnet-runtime-7.0) &>/dev/null
+
+echo "[*] Installing docker and mono-devel"
+apt install -y docker.io mono-devel
+
+echo "[*] Checking if python3 exists"
+(python3 --version) &>/dev/null
+
+if [[ $? == 0 ]]; then
+	echo -n "[*] Found! Version: "
+	python3 --version | cut -d ' ' -f 2
+else
+	echo "[!] Python3 wasn't found. Installing"
+	apt install -y python3 python3-pip
+fi
+
+echo "[*] Installing the libraries required..."
+pip3 install -r requirements.txt
+
+echo "[+] Done!"
