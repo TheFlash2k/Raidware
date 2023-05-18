4415fec92ffc27bebb05ca7404feb80d4a40612d
TheFlash2k <alitaqi2000@gmail.com>
2023-05-19 04:09:35 +0500

    Updated the setup script

--- setup.sh
+++ setup.sh
@@ -7,13 +7,13 @@ fi
 
 echo "[+] Raidware Setup script."
 echo "[+] Setting up mingw:"
-apt install mingw-w64-common mingw-w64-i686-dev mingw-w64-tools mingw-w64-x86-64-dev
+(apt install mingw-w64-common mingw-w64-i686-dev mingw-w64-tools mingw-w64-x86-64-dev -y) &>/dev/null
 
 echo "[+] Setting up CSC for dotnet: "
 echo "[*] Downloading the microsoft prod package"
 (wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb) &>/dev/null
-sudo dpkg -i /tmp/packages-microsoft-prod.deb
-rm /tmp/packages-microsoft-prod.deb
+(sudo dpkg -i /tmp/packages-microsoft-prod.deb) &>/dev/null
+(rm /tmp/packages-microsoft-prod.deb) &>/dev/null
 
 echo "[*] Updating the repositories"
 (apt update) &>/dev/null
@@ -21,7 +21,7 @@ echo "[*] Install dotnet-sdk and dotnet-runtime"
 (apt install -y dotnet-sdk-7.0 dotnet-runtime-7.0) &>/dev/null
 
 echo "[*] Installing docker and mono-devel"
-apt install -y docker.io mono-devel
+(apt install -y docker.io mono-devel) &>/dev/null
 
 echo "[*] Checking if python3 exists"
 (python3 --version) &>/dev/null
@@ -31,7 +31,7 @@ if [[ $? == 0 ]]; then
 	python3 --version | cut -d ' ' -f 2
 else
 	echo "[!] Python3 wasn't found. Installing"
-	apt install -y python3 python3-pip
+	(apt install -y python3 python3-pip) &>/dev/null
 fi
 
 echo "[*] Installing the libraries required..."
